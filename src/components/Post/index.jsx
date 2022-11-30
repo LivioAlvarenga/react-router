fe01a36c41120064911096fce00614e94e70c55c
Livio Alvarenga <livioalvarenga@gmail.com>
2022-11-30 11:21:00 -0300

    add sel e justify center em button

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -8,9 +8,9 @@ export default function Post({ post }) {
           alt="Imagem de capa do post"
         />
       </div>
-      <div className="grid grid-rows-2 items-center   bg-white p-3">
+      <div className="grid grid-rows-2 items-center justify-center  bg-white p-3">
         <h2 className="text-center font-playfair text-xl font-semibold">{post.titulo}</h2>
-        <button className=" justify-self-center rounded-3xl bg-azulLight px-6 py-3 text-xl tracking-wider text-white shadow-md duration-1000  ease-in-out lg:group-hover:bg-azulDark lg:group-hover:px-9  lg:group-hover:shadow-fontPost">
+        <button className=" self-center justify-self-center rounded-3xl bg-azulLight px-6 py-3 text-xl tracking-wider text-white shadow-md duration-1000  ease-in-out lg:group-hover:bg-azulDark lg:group-hover:px-9  lg:group-hover:shadow-fontPost">
           Ler
         </button>
       </div>
