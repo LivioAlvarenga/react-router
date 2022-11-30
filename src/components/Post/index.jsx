9e3cb124b352e712bc89d7a71b99cbfc7f031fbc
Livio Alvarenga <livioalvarenga@gmail.com>
2022-11-30 11:19:20 -0300

    add grip em post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,6 +1,6 @@
 export default function Post({ post }) {
   return (
-    <div className="group grid h-[300px] cursor-pointer grid-rows-2">
+    <div className="group grid h-[320px] cursor-pointer grid-rows-2">
       <div className="overflow-hidden">
         <img
           className="object-cover duration-1000 ease-in-out lg:group-hover:scale-125"
@@ -8,9 +8,9 @@ export default function Post({ post }) {
           alt="Imagem de capa do post"
         />
       </div>
-      <div className="flex flex-col items-center justify-center gap-7 bg-white">
-        <h2 className="font-playfair text-xl font-semibold">{post.titulo}</h2>
-        <button className=" rounded-3xl bg-azulLight px-6 py-3 text-xl tracking-wider text-white shadow-md duration-1000  ease-in-out lg:group-hover:bg-azulDark lg:group-hover:px-9  lg:group-hover:shadow-fontPost">
+      <div className="grid grid-rows-2 items-center   bg-white p-3">
+        <h2 className="text-center font-playfair text-xl font-semibold">{post.titulo}</h2>
+        <button className=" justify-self-center rounded-3xl bg-azulLight px-6 py-3 text-xl tracking-wider text-white shadow-md duration-1000  ease-in-out lg:group-hover:bg-azulDark lg:group-hover:px-9  lg:group-hover:shadow-fontPost">
           Ler
         </button>
       </div>
